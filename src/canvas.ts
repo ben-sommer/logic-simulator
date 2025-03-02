f50edf164d08559fc6c6ad73f74b258ab9b7274b
Ben Sommer <bensommer@myurbanjungle.com>
2025-03-02 17:05:06 +0000

    fix: zoom

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,3 +1,5 @@
+import { setupZoomAndPan } from "./zoomAndPan";
+
 let canvas: HTMLCanvasElement | null = null;
 let ctx: CanvasRenderingContext2D | null = null;
 
@@ -8,10 +10,22 @@ const onResize = () => {
 	canvas.height = window.innerHeight;
 };
 
+const draw = () => {
+	if (!ctx) return;
+
+	ctx.fillStyle = "black";
+	ctx.fillRect(100, 200, 50, 80);
+};
+
 export function setupCanvas(element: HTMLCanvasElement) {
 	canvas = element;
 	ctx = canvas.getContext("2d");
 
 	onResize();
 	window.addEventListener("resize", onResize);
+
+	if (!ctx) return;
+
+	draw();
+	setupZoomAndPan(canvas, ctx, draw);
 }
---
+++ src/zoomAndPan.ts
@@ -0,0 +1,155 @@
+let canvas: HTMLCanvasElement | null = null;
+let ctx: CanvasRenderingContext2D | null = null;
+let draw: (() => void) | null = null;
+
+type Point = {
+	x: number;
+	y: number;
+};
+
+const ORIGIN = Object.freeze({ x: 0, y: 0 });
+
+function diffPoints(p1: Point, p2: Point) {
+	return { x: p1.x - p2.x, y: p1.y - p2.y };
+}
+
+function addPoints(p1: Point, p2: Point) {
+	return { x: p1.x + p2.x, y: p1.y + p2.y };
+}
+
+function scalePoint(p1: Point, scale: number) {
+	return { x: p1.x / scale, y: p1.y / scale };
+}
+
+const ZOOM_SENSITIVITY = 500;
+
+let scale = 1;
+let offset: Point = ORIGIN;
+let mousePos: Point = ORIGIN;
+let viewportTopLeft: Point = ORIGIN;
+let lastMousePos: Point = ORIGIN;
+let lastOffset: Point = ORIGIN;
+let canvasWidth = 0;
+let canvasHeight = 0;
+
+const reset = () => {
+	if (!canvas || !ctx) return;
+
+	ctx.canvas.width = canvasWidth;
+	ctx.canvas.height = canvasHeight;
+	ctx.scale(1, 1);
+	scale = 1;
+
+	mousePos = ORIGIN;
+	viewportTopLeft = ORIGIN;
+	lastMousePos = ORIGIN;
+	lastOffset = ORIGIN;
+
+	applyTransform();
+	applyPan();
+};
+
+const onMouseMove = (event: MouseEvent) => {
+	const currentMousePos = {
+		x: event.pageX,
+		y: event.pageY,
+	};
+
+	const mouseDiff = diffPoints(currentMousePos, lastMousePos);
+	offset = addPoints(offset, mouseDiff);
+	applyTransform();
+	applyPan();
+	lastMousePos = currentMousePos;
+};
+
+const onMouseUp = () => {
+	document.removeEventListener("mousemove", onMouseMove);
+	document.removeEventListener("mouseup", onMouseUp);
+};
+
+const startPan = (event: MouseEvent) => {
+	document.addEventListener("mousemove", onMouseMove);
+	document.addEventListener("mouseup", onMouseUp);
+	lastMousePos = { x: event.pageX, y: event.pageY };
+};
+
+const applyPan = () => {
+	if (!ctx) return;
+
+	const offsetDiff = scalePoint(diffPoints(offset, lastOffset), scale);
+
+	ctx.translate(offsetDiff.x, offsetDiff.y);
+	viewportTopLeft = diffPoints(viewportTopLeft, offsetDiff);
+	lastOffset = offset;
+};
+
+const applyTransform = () => {
+	if (!ctx) return;
+
+	const storedTransform = ctx.getTransform();
+	ctx.canvas.width = ctx.canvas.width;
+	ctx.setTransform(storedTransform);
+
+	if (draw) draw();
+};
+
+const onMouseUpdate = (event: MouseEvent) => {
+	if (!canvas) return;
+
+	const viewportMousePos = {
+		x: event.clientX,
+		y: event.clientY,
+	};
+	const topLeftCanvasPos = {
+		x: canvas.offsetLeft,
+		y: canvas.offsetTop,
+	};
+
+	mousePos = diffPoints(viewportMousePos, topLeftCanvasPos);
+};
+
+const applyZoom = (event: WheelEvent) => {
+	if (!ctx) return;
+
+	const zoom = 1 - event.deltaY / ZOOM_SENSITIVITY;
+	const viewportTopLeftDelta = {
+		x: (mousePos.x / scale) * (1 - 1 / zoom),
+		y: (mousePos.y / scale) * (1 - 1 / zoom),
+	};
+	const newViewportTopLeft = addPoints(viewportTopLeft, viewportTopLeftDelta);
+
+	ctx.translate(viewportTopLeft.x, viewportTopLeft.y);
+	ctx.scale(zoom, zoom);
+	ctx.translate(-newViewportTopLeft.x, -newViewportTopLeft.y);
+
+	viewportTopLeft = newViewportTopLeft;
+	scale *= zoom;
+
+	applyPan();
+	applyTransform();
+};
+
+const onResize = () => {
+	reset();
+	applyTransform();
+};
+
+const onWheel = (event: WheelEvent) => {
+	onMouseUpdate(event);
+	applyZoom(event);
+};
+
+export const setupZoomAndPan = (
+	element: HTMLCanvasElement,
+	context: CanvasRenderingContext2D,
+	drawFunction: () => void
+) => {
+	canvas = element;
+	ctx = context;
+	draw = drawFunction;
+
+	document.addEventListener("resize", onResize);
+	document.addEventListener("mousemove", onMouseUpdate);
+	document.addEventListener("wheel", onWheel);
+	document.addEventListener("mousedown", startPan);
+};
